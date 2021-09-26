7f0d431c61f90f0c659920eb67a0461ea14cb985
Troy Pappas <troypappas147@gmail.com>
2021-09-26 00:02:38 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, send_from_directory, abort
+from project_data import ProjectPortfolio, ProjectApp, ProjectMap, ProjectNLP
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    portfolio_project = ProjectPortfolio()
+    map_project = ProjectMap()
+    return render_template('index.html', portfolio_project=portfolio_project, map_project=map_project)
+
+@app.route('/project/<name>')
+def project(name):
+    # project_data = ProjectData(name)
+    # return render_template('project.html', project_data=project_data)
+    if name == 'portfolio':
+        project_data = ProjectPortfolio()
+        return render_template('project.html', project_data=project_data)
+    elif name == 'map':
+        project_data = ProjectMap()
+        return render_template('project.html', project_data=project_data)
+    return render_template('unavailable.html')
+
+@app.route("/resume_download")
+def resume_download():
+    try:
+        basepath = os.path.join(os.path.dirname(__file__), 'static')
+    except Exception:
+        abort(404)
+    return send_from_directory(basepath, 'troy_pappas_resume.pdf', as_attachment=True)
---
+++ project_data.py
@@ -0,0 +1,41 @@
+class ProjectPortfolio():
+    available = True
+    title = "Portfolio"
+    index_desc = "The project page for the site you're currently viewing."
+    live_link = "https://troypappas.azurewebsites.net/"
+    code_link = ""
+    intro_text = ""
+    primary_text = ""
+    tools_used = []
+
+class ProjectMap():
+    available = False
+    title = ""
+    index_desc = ""
+    live_link = ""
+    code_link = ""
+    intro_text = ""
+    primary_text = ""
+    tools_used = []
+
+class ProjectApp():
+    available = False
+    title = "LoreGate"
+    index_desc = "An independent proof-of-concept I came up with to guide me through learning the Flask framework."
+    live_link = ""
+    code_link = ""
+    intro_text = ""
+    primary_text = ""
+    tools_used = []
+
+class ProjectNLP():
+    available = False
+    title = ""
+    index_desc = ""
+    live_link = ""
+    code_link = ""
+    intro_text = ""
+    primary_text = ""
+    tools_used = []
+
+
