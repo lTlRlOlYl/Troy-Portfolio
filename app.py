40e817938d244fb19bc41eea3f66274902a40b2c
Troy Pappas <troypappas147@gmail.com>
2021-10-04 12:10:03 -0400

    added automation project

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, send_from_directory, abort, url_for
-from project_data import ProjectPortfolio, ProjectApp, ProjectMap, ProjectNLP
+from project_data import ProjectPortfolio, ProjectMap, ProjectAutomation
 
 app = Flask(__name__)
 
@@ -8,10 +8,12 @@ app = Flask(__name__)
 def index():
     portfolio_project = ProjectPortfolio()
     map_project = ProjectMap()
+    automation_project = ProjectAutomation()
     return render_template(
         'index.html',
         portfolio_project=portfolio_project,
         map_project=map_project,
+        automation_project=automation_project,
         )
 
 @app.route('/project/<name>')
@@ -22,6 +24,9 @@ def project(name):
     elif name == 'map':
         project_data = ProjectMap()
         return render_template('map_project.html', project_data=project_data)
+    elif name == 'automation':
+        project_data = ProjectAutomation()
+        return render_template('automation_project.html', project_data=project_data)
     return render_template('unavailable.html')
 
 @app.route("/resume_download")
--- project_data.py
+++ project_data.py
@@ -5,12 +5,6 @@ class ProjectPortfolio():
     index_desc = "The project page for the site you're currently viewing."
     live_link = "https://troypappas.azurewebsites.net/"
     code_link = "https://github.com/lTlRlOlYl/Troy-Portfolio"
-    asset_url = './static/assets/png/portfolio-mockup-2.png'
-#     intro_text = '''
-# A portfolio project based on the
-# <a href="https://dopefolio.netlify.app/">Dopefolio</a> template by
-# <a href="https://rammaheshwari.com">Ram Maheshwari</a>.
-# '''
     intro_text = '''
 A portfolio project based on the Dopefolio template by Ram Maheshwari.
 '''
@@ -28,7 +22,7 @@ and hosted it on Azure's app service.
         "HTML",
         "CSS",
         "JavaScript",
-        "Azure"
+        "Azure App Service"
     ]
 
 
@@ -37,7 +31,6 @@ class ProjectMap():
     index_desc = "An interactive map of election costs in Massachusetts municipalities."
     live_link = "https://public.tableau.com/app/profile/troy.pappas/viz/ExampleVisualization/Sheet12"
     code_link = ""
-    asset_url = './static/assets/png/map-mockup-2.png'
     intro_text = '''
 A map of historical election costs for the Commonwealth of Massachusetts, 
 with inbuilt functionality for switching between measures, adjusting the gradient, and more.
@@ -59,29 +52,73 @@ Parameters were leveraged in Tableau to implement the measure and gradient contr
     ]
 
 
-class ProjectApp():
-    title = "LoreGate"
-    index_desc = "An independent proof-of-concept I came up with to guide me through learning the Flask framework."
+
+class ProjectAutomation():
+    title = "Business Process Automation"
+    index_desc = "A solution that populates DevOps based on a custom business framework using data from other systems."
     live_link = ""
-    code_link = ""
-    intro_text = ""
+    code_link = "https://github.com/lTlRlOlYl/TFS-Automation"
+    intro_text = '''
+An end-to-end streamlining of a business framework for a data analytics team.
+'''
     primary_text = [
 '''
+The organization that this case is based on uses a number of systems to manage their operational data. 
+This solution uses a python program to scan these systems for the initialization of new projects, 
+then creates a DevOps workflow based on the data analytics unit's business framework.
 ''',
-    ]
-    tools_used = []
-
-
-class ProjectNLP():
-    title = ""
-    index_desc = ""
-    live_link = ""
-    code_link = ""
-    intro_text = ""
-    primary_text = [
 '''
+The python program is designed to create a workflow based on new projects in DevOps as soon as they're created in the other systems by 
+users outside the data analytics unit. The workflow involves more than fifty work items in a hierarchy structure, 
+that need to be assigned based on file system data. The program sends detailed notification emails for workflow creation and assignment, 
+as well as a dynamically populated MS Word template.
+''',
+'''
+This solution also involves ETL from the DevOps operational data store and other systems 
+to create an integrated data model using SQL Server. A hosted Power BI report built off the data model 
+provides a streamlined and fully integrated view of all the work items, with color for status indication and hyperlinks 
+to the work items for fast and intuitive navigation.
+''',
+'''
+The solution enables accurate and timely reporting on the data analytics unit's metrics, 
+in additional to saving hundreds of hours of manual data entry.
+''',
+'''
+There's no active link, but feel free to check out the code on GitHub using the button below. 
+Parts of the solution have been omitted to protect the organization's privacy.
 ''',
     ]
-    tools_used = []
+    tools_used = [
+        "Python",
+        "Azure DevOps",
+        "SQL Server",
+        "Power BI"
+    ]
+
+
+# class ProjectApp():
+#     title = "LoreGate"
+#     index_desc = "An independent proof-of-concept I came up with to guide me through learning the Flask framework."
+#     live_link = ""
+#     code_link = ""
+#     intro_text = ""
+#     primary_text = [
+# '''
+# ''',
+#     ]
+#     tools_used = []
+
+
+# class ProjectNLP():
+#     title = ""
+#     index_desc = ""
+#     live_link = ""
+#     code_link = ""
+#     intro_text = ""
+#     primary_text = [
+# '''
+# ''',
+#     ]
+#     tools_used = []
 
 
